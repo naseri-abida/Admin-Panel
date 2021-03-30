238814dc3750587af9c3dc8b1a66fc5a92af4713
naseri-abida <israrullah.zaheer786@gmail.com>
2021-03-30 21:43:56 +0430

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'abida',
+            'email' => 'abida@gmail.com',
+            'password' => Hash::make('abida123'),
+        ]);
+    }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="copyright">
+            &copy; 2021 Laravel Admin Panel
+        </div>
+    </div>
+</footer>
